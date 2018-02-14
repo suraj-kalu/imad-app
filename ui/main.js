8b42c7b660e03fe2b47d756d80580cd1aafc8044
suraj-kalu <suraz.kalu@gmail.com>
2018-02-14 12:23:06 +0545

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,6 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
  }
 
- img.onClick = function(){
+ img.onclick = function(){
      var interval= setInterval(moveRight,50);
  };
