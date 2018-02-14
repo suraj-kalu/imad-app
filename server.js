6b4a884a28565baa8985d5ca768282900b91ef56
suraj-kalu <suraz.kalu@gmail.com>
2018-02-14 11:25:46 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,8 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     /* articleName = article-one */
     /*article[articleName] = {} content object for article one */
+    
+    var articleName = req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
 
