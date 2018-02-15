1556a2505cc86fd4739a07ddfd6c2975a71872c2
suraj-kalu <suraz.kalu@gmail.com>
2018-02-15 10:03:23 +0545

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,20 +31,35 @@ button.onclick=function(){
 
 //Submit name
 
-var nameInput= document.getElementbyId('name');
-var name= nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
 //Make request to server and send the name
-
-//capture the list of names and render it to the list
+//Make a request to counter end point
+ 
+  var request = new XMLHttpRequest();
+  
+  
+  //Capture the respose and store it into the variable
+  
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE ){
+          if(request.status === 200){
+            //capture the list of names and render it to the list
 var names =['name1','name2','name3'];
 var list = '';
 for(var i =0; i< names.length;i++){
     list += '<li>' + names[i] + '</li>';
-}
+      }
+      var ul = document.getElementbyId('nameList');
+  }
+  }
 
-var ul = document.getElementbyId('nameList');
+};
+
+var nameInput= document.getElementbyId('name');
+var name= nameInput.value;
+   request.open('GET','http://surajratnakalu.imad.hasura-app.io/submit-name?name='+ name,true);
+   request.send(null);
 
 
 };
