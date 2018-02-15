77458cdf91f3241142b14042d16661631993656a
suraj-kalu <suraz.kalu@gmail.com>
2018-02-15 08:49:52 +0545

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'New value';
+// var element = document.getElementById('main-text');
+// element.innerHTML = 'New value';
 
- var img = document.getElementById('madi');
+//  var img = document.getElementById('madi');
 
- var marginLeft =0;
+//  var marginLeft =0;
 
-function moveRight(){
-     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
- }
+// function moveRight(){
+//      marginLeft = marginLeft + 10;
+//     img.style.marginLeft = marginLeft + 'px';
+//  }
 
- img.onclick = function(){
-     var interval= setInterval(moveRight,50);
- };
+//  img.onclick = function(){
+//      var interval= setInterval(moveRight,50);
+//  };
