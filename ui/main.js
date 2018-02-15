60d7835f26a1d9e6575080a1f273a602f76bb428
suraj-kalu <suraz.kalu@gmail.com>
2018-02-15 09:40:27 +0545

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,31 @@ console.log('Loaded!');
 //  img.onclick = function(){
 //      var interval= setInterval(moveRight,50);
 //  };
+
+var button = document.getElementById('counter');
+
+button.onclick=function(){
+    
+ 
+ //Make a request to counter end point
+ 
+  var request = new XMLHttpRequest();
+  
+  
+  //Capture the respose and store it into the variable
+  
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE ){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+      }
+  }
+  
+  };
+    //Make request
+    
+   request.open('GET','http://surajratnakalu.imad.hasura-app.io/counter',true);
+   request.send(null);
+};
