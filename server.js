9e46122b6f95e3e410363fe8f70ffd23c938a786
suraj-kalu <suraz.kalu@gmail.com>
2018-02-22 09:34:43 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/test-db',function(req, res){
    
    pool.query('SELECT * FROM test',function(err,result){
        if(err){
-           res.status(500).err.toString();
+           res.status(500).send(err.toString());
        }
        else{
            res.send(JSON.stringify(result));
