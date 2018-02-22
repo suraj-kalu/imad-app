043ce87adb69e4c40bd5392dd0490aa38837b263
suraj-kalu <suraz.kalu@gmail.com>
2018-02-22 09:46:40 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ return htmlTemplate;
 var pool = new Pool(config);
 app.get('/test-db',function(req, res){
    
-   pool.query('SELECT * FROM tesst',function(err,result){
+   pool.query('SELECT * FROM test',function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }
