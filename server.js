3adf0523e2cb1f9e46191bd5d253ae4fbd2f8a3a
suraj-kalu <suraz.kalu@gmail.com>
2018-02-28 07:31:05 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,12 +133,22 @@ app.get('/comments',function(req,res){
 });
 
 
-app.get('/:articleName', function (req, res) {
-    /* articleName = article-one */
-    /*article[articleName] = {} content object for article one */
-    
-    var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
+
+
+app.get('/articles/:articleName', function (req, res){
+
+    pool.query("SELECT * FROM article WHERE title = '" +req.params.articleName +"'", function (err,result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length===0) {
+               res.status(400).send('Article Not found');
+           } else {
+               var articleData=result.row[0];
+               res.send(createTemplate(articleData)); 
+           }
+       }
+    });
 });
 
 app.get('/article-two', function (req, res) {
